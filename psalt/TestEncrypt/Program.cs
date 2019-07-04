7effa9803b96ebf86b2b3d18aef983c2015cba9d
cogentpages <evalega@gmail.com>
2019-07-04 16:54:37 +0800

    Add files via upload

---
+++ psalt/TestEncrypt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using psalt;
+
+namespace TestEncrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing encryption. Press Enter to proceed..");
+            Console.ReadLine();
+            var saltPass = new Psalt();
+
+            var username = "User";
+            var password = "pass";
+
+            Console.WriteLine("Encrypting password for User: " + username + "...");
+            Console.WriteLine("Old Password: " + password);
+            Console.WriteLine("New Password: " + saltPass.EncryptPassword(username, password));
+
+            username = "User2";
+            password = "pass";
+
+            Console.WriteLine("Encrypting password for User: " + username + "...");
+            Console.WriteLine("Old Password: " + password);
+            Console.WriteLine("New Password: " + saltPass.EncryptPassword(username, password));
+
+            username = "User3";
+            password = "pass";
+
+            Console.WriteLine("Encrypting password for User: " + username + "...");
+            Console.WriteLine("Old Password: " + password);
+            Console.WriteLine("New Password: " + saltPass.EncryptPassword(username, password));
+
+            Console.WriteLine("done.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ psalt/psalt/Psalt.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace psalt
+{
+    public class Psalt 
+    {
+        private string SaltPath { get; set; }
+        private List<UserSalt> ExistingSalts { get; set; }
+
+        /// <summary>
+        /// Initialize Psalt with the desired path for the collection of salts. If path is not defined, this will be saved to location of the application.
+        /// </summary>
+        /// <param name="saltFilePath"></param>
+        public Psalt(string saltFilePath = null)
+        {
+            SaltPath = Path.Combine(string.IsNullOrEmpty(saltFilePath) ? 
+                                    Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) : 
+                                    saltFilePath, 
+                                    "salt.dat");
+        }
+
+        private string GetUserSalt(string userName)
+        {
+            string userSalt = "";
+
+            if (File.Exists(SaltPath))
+            {
+                using (StreamReader r = new StreamReader(SaltPath))
+                {
+                    var json = r.ReadToEnd();
+                    ExistingSalts = JsonConvert.DeserializeObject<List<UserSalt>>(json);
+                    userSalt = ExistingSalts?.Where(u => u == null || u.Username == userName).Select(a => a.Salt).SingleOrDefault();
+                }
+            }
+            return userSalt;
+        }
+
+        private void SetNewUserSalt(string userName, string newSalt)
+        {
+            var userSalt = new UserSalt
+            {
+                Username = userName,
+                Salt = newSalt
+            };
+
+            string newContent = JsonConvert.SerializeObject(userSalt);
+
+            using(StreamWriter r = new StreamWriter(SaltPath, false))
+            {
+                if(ExistingSalts == null)
+                {
+                    ExistingSalts = new List<UserSalt>
+                    {
+                        userSalt
+                    };
+                }
+                else
+                    ExistingSalts.Add(userSalt);
+
+                r.WriteLine(JsonConvert.SerializeObject(ExistingSalts));
+
+            }
+        }
+
+        private string GenerateSalt()
+        {
+            byte[] bytes = new byte[128 / 8];
+            using (var keyGenerator = RandomNumberGenerator.Create())
+            {
+                keyGenerator.GetBytes(bytes);
+                return BitConverter.ToString(bytes).Replace("-", "").ToLower();
+            }
+        }
+
+        /// <summary>
+        /// Generates encrypted password with salt. If the user exists with the salt, then the password will be generated along with the saved salt.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns>Encrypted Password</returns>
+        public string EncryptPassword(string username, string password)
+        {
+            var userSalt = GetUserSalt(username);
+
+            if (string.IsNullOrEmpty(userSalt)){
+                userSalt = GenerateSalt();
+                SetNewUserSalt(username, userSalt);
+            }
+            
+            var newPassword = userSalt + password;
+
+            UTF8Encoding encoder = new UTF8Encoding();
+            SHA256CryptoServiceProvider hasher = new SHA256CryptoServiceProvider();
+            return BitConverter.ToString(hasher.ComputeHash(encoder.GetBytes(newPassword))).Replace("-", "");
+        }
+    }
+
+    public class UserSalt
+    {
+        public string Username { get; set; }
+        public string Salt { get; set; }
+    }
+}
